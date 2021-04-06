d146d66e207cf2f7111aae27e95e5c874dc8feee
Cosmic Swap <cosmicswap0@gmail.com>
2021-04-06 02:27:21 +0100

    compiled it lol

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2757,7 +2757,7 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#7F2D00",
-    primary: "#2d50e6",
+    primary: "#814fc1",
     primaryBright: "#2686FC",
     primaryDark: "#003ECF",
     secondary: "#0088F6",
@@ -2767,7 +2767,7 @@ var baseColors = {
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#fcf7e8", backgroundDisabled: "#e9eaeb", contrast: "#191326", invertedContrast: "#f2f2f2", input: "#eeeaf4", tertiary: "#d5dcfa", text: "#12205c", textDisabled: "#8196f0", textSubtle: "#1b308a", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#f8f0ff", backgroundDisabled: "#e9eaeb", contrast: "#191326", invertedContrast: "#f2f2f2", input: "#eeeaf4", tertiary: "#d5dcfa", text: "#12205c", textDisabled: "#8196f0", textSubtle: "#1b308a", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#FFFFFF", background: "#0d1845", backgroundDisabled: "#0d1845", contrast: "#FFFFFF", invertedContrast: "#2948cf", input: "#eeeaf4", primaryDark: "#003ECF", tertiary: "#4262e9", text: "#ffffff", textDisabled: "#5773eb", textSubtle: "#abb9f5", borderColor: "#09102e", card: "#1b308a", gradients: {
